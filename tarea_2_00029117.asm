3936c67bc2c3993d7f06639e3277ea1f0301d25a
cortezle <00029117@uca.edu.sv>
2020-04-25 23:21:08 -0600

    ejercicio 2 dinamico

--- tarea_2_00029117.asm
+++ tarea_2_00029117.asm
@@ -19,6 +19,8 @@ section .text
 		;Limpiando antes de usar otra vez
 		mov cx, 0h
 		mov di, 0d
+
+		;longitud loop
 		mov cx, 11d
 
 		;imprimiendo valores
@@ -30,6 +32,27 @@ euno:	mov bx, [comnt+di]
 		inc di
 		loop euno
 
+
+		;Ejercicio 2
+
+		;limpiando valores
+		mov ax, 0h
+		mov bx, 0h
+		mov cx, 0h
+		mov di, 0d
+
+		;Asignando valores claves
+		mov ax, 4h ;inicio 4 casos
+		mov bx, 2h ;se duplican los casos
+		mov cx, 000bh ;11 resultados
+
+edos:	mov [di+210h],ax
+		mul bx
+		inc di
+		loop edos
+
+		
+		
 		int 20h
 
 section .data
